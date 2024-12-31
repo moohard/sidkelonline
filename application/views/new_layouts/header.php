58323404b170d4a9c62061b3fc58f6ad46b85c6b
Muhardiansyah <dian.kuba13@gmail.com>
2024-12-31 22:43:42 +0800

    tes

--- application/views/new_layouts/header.php
+++ application/views/new_layouts/header.php
@@ -1,16 +1,17 @@
 <!--begin::Header-->
-<div id="kt_header" class="header align-items-stretch mb-5 mb-lg-10" data-kt-sticky="true" data-kt-sticky-name="header" data-kt-sticky-offset="{default: '200px', lg: '300px'}">
-	<!--begin::Container-->
-	<div class="container-xxl d-flex align-items-center">
-		<!--begin::Header Logo-->
-		<div class="header-logo me-5 me-md-10 flex-grow-1 flex-lg-grow-0">
-			<a href="index.html">
-				<img alt="Logo" src="<?= base_url() ?>assets/media/logos/demo2.png" class="logo-default h-25px" />
-				<img alt="Logo" src="<?= base_url() ?>assets/media/logos/demo2-sticky.png" class="logo-sticky h-25px" />
-			</a>
-		</div>
-		<!--end::Header Logo-->
-	</div>
-	<!--end::Container-->
+<div id="kt_header" class="header align-items-stretch mb-5 mb-lg-10" data-kt-sticky="true" data-kt-sticky-name="header"
+  data-kt-sticky-offset="{default: '200px', lg: '300px'}">
+  <!--begin::Container-->
+  <div class="container-xxl d-flex align-items-center">
+    <!--begin::Header Logo-->
+    <div class="header-logo me-5 me-md-10 flex-grow-1 flex-lg-grow-0">
+      <a href="index.html">
+        <img alt="Logo" src="<?= base_url() ?>assets/media/logos/logo-penajam.webp" class="logo-default h-50px" />
+        <img alt="Logo" src="<?= base_url() ?>assets/media/logos/logo-penajam.webp" class="logo-sticky h-50px" />
+      </a>
+    </div>
+    <!--end::Header Logo-->
+  </div>
+  <!--end::Container-->
 </div>
 <!--end::Header-->
